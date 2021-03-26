58d0abe2323bfe88648ce5a729a35a4d91a6df41
ffaamunchy <51778797+ffaamunchy@users.noreply.github.com>
2021-03-26 17:37:39 +0800

    Add files via upload

--- helloworld.java
+++ helloworld.java
@@ -1,34 +1,38 @@
 import java.util.*;
 import java.util.stream.IntStream;
 public class helloworld {
-	//private String[] state = {"kuala terengganu","kuala lumpur","kajang","bangi"};
+    //private String[] state = {"kuala terengganu","kuala lumpur","kajang","bangi"};
     // Function return true if given element
     // found in array
-    int kodJenisLain=0;
-    int kodJenisNoRumah=1;
-    int kodJenisNegeri=2;
+    
 
-    public  static String[] state = new String[]{"Kuala TERengganu","kuala lumpur","kajang","bangi"};
+    public  static String[] city = new String[]{"Kuala TERengganu","kuala lumpur","kajang","bangi"};
+    public  static String[] state = new String[]{"Selangor","Terengganu","Pahang","Chendering"};
     
     public static boolean isNoRumah(String strValue)
     {
-        
+            String[] isDigit = strValue.split(" "); // [No, 11]
+            //System.out.println("Value no rumah : "+ Arrays.toString(isDigit));
+            for (int i = 0; i < isDigit.length; i++) {
+
+                /*if(isDigit[i].startsWith("No")){
+                    System.out.println(isDigit[i]+" ");
+                    
+                    return true;
+                }*/
+                String aptVal = isDigit[i].toLowerCase();
+                if (aptVal.startsWith("no") || isNumber(isDigit[i])) {
+                    //System.out.println("'apt':" + strValue);
+                    
+                    return true;
+                } 
+                
+            }
+
+
          
-	        for (int i = 0; i < state.length; i++) {
-	        	
-	        	boolean test = strValue.contains(state[i].toLowerCase());
-	        	if (test){
-	        		// Print the result
-	        		return true;
-
-	        	}
-	        	else {
-
-	        		System.out.println("not sames");
-	        		return false;
-	        	}
-	        }
-  		
+            
+        
         return false;
     }
     //--------------------------------------------------------------------------
@@ -36,124 +40,116 @@ public class helloworld {
     {
         
          
-	        for (int i = 0; i < state.length; i++) {
-	        	
-	        	boolean test = strValue.contains(state[i].toLowerCase());
-	        	if (test){
-	        		// Print the result
-	        		return true;
+            for (int i = 0; i < city.length; i++) {
+                
+                boolean match = strValue.contains(city[i].toLowerCase());
+                if (match){
+                    // Print the result
+                    return true;
 
-	        	}
-	        	else {
+                }
+                else {
 
-	        		System.out.println("not sames");
-	        		return false;
-	        	}
-	        }
-  		
+                    //System.out.println("not sames");
+                    return false;
+                }
+            }
+        
+        return false;
+    }
+    //--------------------------------------------------------------------------
+    public static boolean isDaerah(String strValue)
+    {
+        
+         
+            for (int i = 0; i <= state.length; i++) {
+                System.out.println("****"+state[i]);
+                boolean match = strValue.contains(state[i].toLowerCase());
+                if (match){
+                    // Print the result
+                    //System.out.println(match1);
+                    return true;
+
+                }
+                else {
+                    return false;
+                }
+            }
+        
         return false;
     }
 
+    public static boolean isNumber(String val){
+        int intValue;
+            try {
+            intValue = Integer.parseInt(val);
+            return true;
+        } catch (NumberFormatException e) {
+            //System.out.println("Input String cannot be parsed to Integer.");
+        }
+        return false;
+    }
    public static void main(String[]args){
-   	// Get the array
-        //Integer arr[] = { 5, 1, 1, 9, 7, 2, 6, 10 };
-  
-        // Get the value to be checked
-        //int toCheckValue = 7;
+        String kodJenisLain="";
+        String kodJenisNoRumah="";
+        String kodJenisNegeri="";
+        String kodJenisDaerah="";
         //Scanner input = new Scanner(System.in);
-	   	//System.out.printf("Enter your address : "); //No 11, Chendering, 21080 Kuala Terengganu, Terengganu.
-	   	//String address = input.nextLine();
-	   	String address = "Kuala Terengganu, Kuala Terengganu, Chendering";
-	   	String[] toCheckValue = address.split(","); 
-        int[] kodType = new int[toCheckValue.length];
+        //System.out.printf("Enter your address : "); //No 11, Chendering, 21080 Kuala Terengganu, Terengganu.
+        //String address = input.nextLine();
+        
 
-
-		//String[] arr = Arrays.toString(tokens); //[No 11,  Kuala Terengganu,  Chendering]
-  		//String[] arr = new String[]{"Kuala Terengganu","Kuala Lumpur","Kajang","Bangi"};
-        // Print the array
-        //System.out.println("Array: " + Arrays.toString(toCheckValue)); //[No 11,  Kuala Terengganu,  Chendering]
-        //System.out.println("Array: " + Arrays.toString(arr)); 
-  
-        // Check if this value is
-        // present in the array or not
-
+        String address = "no F-3A-16, Kuala Terengganu, Pahang";
+        String[] toCheckValue = address.split(","); // [No 11, Kuala Terengganu, Chendering]
+        String[] kodType = new String[toCheckValue.length];
+        //toCheckValue.toLowerCase();
         for (int a = 0; a < toCheckValue.length; a++) 
         {
-        	if (isNoRumah(toCheckValue[a].toLowerCase()) {
-        		kodType[0]=kodJenisNoRumah;
-        	}
-        	else
-        	{
-        		kodType[0]=kodJenisLain;
-        	}
-        	//---------------
-        	if (isBandar(toCheckValue[a].toLowerCase()) {
-        		kodType[1]=kodJenisNoRumah;
-        	}
-        	else
-        	{
-        		kodType[1]=kodJenisLain;
-        	}
-    	}
 
-    /*String number;
-     String address;
-     String aptNumber;
+            /*if(toCheckValue[a].startsWith("No") || toCheckValue[a].startsWith("no")){
+                isNoRumah(toCheckValue[a]);
+            }*/
+            //---------------
+            if (toCheckValue[a].startsWith("No") || toCheckValue[a].startsWith("no")) {
+                if(isNoRumah(toCheckValue[a])){
+                    kodJenisNoRumah = toCheckValue[a];
+                    kodType[0] = kodJenisNoRumah;
+                }
+            }else
+            {
+                kodType[0]=kodJenisLain;
+            }
+            
+            //---------------
+            if (isBandar(toCheckValue[a].toLowerCase())) {
+                kodJenisNegeri = toCheckValue[a];
+                kodType[1]=kodJenisNegeri;
+                //String kodJenisNegeri = kodType[1];
+            }
+            else
+            {
+                kodType[1]=kodJenisLain;
+                //String kodJenisLain = kodType[1];
+            }
 
+            //---------------
+            //isDaerah(toCheckValue[a].toLowerCase());
+            if (isDaerah(toCheckValue[a].toLowerCase())) {
+                System.out.println(toCheckValue[a]);
+                kodJenisDaerah = toCheckValue[a];
+                kodType[2]=kodJenisDaerah;
+                //String kodJenisNegeri = kodType[1];
+            }
+            else
+            {
+                kodType[2]=kodJenisLain;
+                //String kodJenisLain = kodType[1];
+            }
+            //System.out.printf("'apt : '"+kodType[0]+"\n 'city : '"+kodType[1]+"\n 'state : '"+kodType[2]+"\n");
+            //System.out.printf(kodType[a]);
+            
+        }
 
-    String str = "No 11, Chendering, 21080 Kuala Terengganu, Terengganu.";
-    //String str = "This is String , split by StringTokenizer";
-    StringTokenizer st = new StringTokenizer(str);
-
-    System.out.println("---- Split by space ------");
-    System.out.println(Arrays.toString(st));
-    while (st.hasMoreElements()) {
-                String s = System.out.println(st.nextElement());
-
-                if(StringUtils.isNumeric(s)){
-                    number = s;
-                    continue;  
-            	}   
-
-                //if(s.string("Apt")) {
-                //   aptNumber = s.substring(s.indexOf("Apt"),s.length-1);
-                //   continue;
-                //}
-    }
-
-    System.out.println("---- Split by comma ',' ------");
-    StringTokenizer st2 = new StringTokenizer(str, ",");
-
-    while (st2.hasMoreElements()) {
-        System.out.println(st2.nextElement());
-    }*/
-
-
-    /*Scanner input = new Scanner(System.in);
-   	//helloworld s = new helloworld();
-
-   	System.out.printf("Enter your address : ");
-   	String address = input.nextLine();
-
-   	//System.out.println(" Student is " +name);
-
-   	//String test = "It's the number 1 way.";
-	String[] tokens = address.split(",");       // Single blank is the separator. {"A", "C"}
-	System.out.println(Arrays.toString(tokens));
-
-	String[] values = {"Kuala Terengganu","Kuala Lumpur","Kajang","Bangi"};
-	boolean contains = Arrays.stream(tokens).anyMatch(s -> s.contains(Arrays.asList(values)));
-	System.out.println(contains);
-	if (contains){
-		System.out.println(Arrays.stream(tokens).anyMatch(address::contains));
-	}
-	
-	//var p5 = new Person(27, "Lucy", Gender.FEMALE);
-
-    */
-
-
-	
-
+        
    }
 }
